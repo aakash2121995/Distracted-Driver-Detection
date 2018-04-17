03c0305cca3623b1abe2005a2f37dc77430a3d11
EC2 Default User <ec2-user@ip-172-31-1-206.us-west-2.compute.internal>
2018-04-17 05:43:23 +0000

    Adding code and notebooks

---
+++ Convolution NN without pretrained weights -16.py
@@ -0,0 +1,165 @@
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.optimizers import SGD
+from keras.models import load_model
+from keras import backend as K
+K.set_image_dim_ordering('th')
+import numpy as np
+import os
+import glob
+import cv2
+import pickle
+import datetime
+import pandas as pd
+import time
+from shutil import copy2
+import warnings
+warnings.filterwarnings("ignore")
+from numpy.random import permutation
+np.random.seed(2016)
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+from keras.utils import np_utils
+from keras.models import model_from_json
+from keras.models import load_model
+import h5py
+
+img_width = 224
+img_height = 224
+
+def VGG_16():
+    model = Sequential()
+    model.add(ZeroPadding2D((1, 1), input_shape=(3, 224, 224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+
+    print('Model loaded.')
+
+    model.add(Dense(10, activation='softmax'))
+
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+class EarlyStoppingByLossVal(Callback):
+    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            warnings.warn("Early stopping requires %s available!" % self.monitor, RuntimeWarning)
+
+        if current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
+
+model = VGG_16()
+
+def preProcess(x):
+    mean_pixel = [103.939, 116.779, 123.68]
+    x[0, :, :] -= mean_pixel[0]
+    x[1, :, :] -= mean_pixel[1]
+    x[2, :, :] -= mean_pixel[2]
+    return x
+
+
+# this is the augmentation configuration we will use for training
+train_data_dir = "Training Data/"
+validation_data_dir = "Validation Data/"
+train_datagen = ImageDataGenerator(
+	                rotation_range=20,
+	                width_shift_range=0.2,
+	                height_shift_range=0.2,
+	                shear_range=0.1,
+	                zoom_range=0.1,
+	                horizontal_flip=False,
+	                fill_mode='nearest',
+                    preprocessing_function = preProcess)
+
+# # this is the augmentation configuration we will use for testing:
+# # only rescaling
+# train_data_dir = '../Labelled Train Data/'
+test_datagen = ImageDataGenerator(preprocessing_function = preProcess)
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+
+
+# In[ ]:
+
+callbacks = [
+        EarlyStoppingByLossVal(monitor='val_loss', value=0.00001, verbose=1),
+        EarlyStopping(monitor='val_loss', patience=5, verbose=0)
+    ]
+
+for e in range(50):
+    print("Epoch {}".format(e+1))
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=15137,
+        nb_epoch=1,
+        validation_data=validation_generator,
+        nb_val_samples=1681,
+        callbacks = callbacks
+        )
+    model.save("weights-16 without pretraining.h5")
+    
---
+++ Convolutional+Neural+Networks-19.py
@@ -0,0 +1,211 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.optimizers import SGD
+from keras.models import load_model
+from keras import backend as K
+K.set_image_dim_ordering('th')
+import numpy as np
+import os
+import glob
+import cv2
+import pickle
+import datetime
+import pandas as pd
+import time
+from shutil import copy2
+import warnings
+warnings.filterwarnings("ignore")
+from numpy.random import permutation
+np.random.seed(2016)
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
+from keras.utils import np_utils
+from keras.models import model_from_json
+from keras.models import load_model
+import h5py
+
+
+# dimensions of our images.
+img_width = 224
+img_height = 224
+
+
+# nb_train_samples = 7102
+# nb_validation_samples = 198
+# nb_epoch = 5
+
+# model = load_model("modelFew.h5")
+
+
+# In[2]:
+
+def VGG_19():
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+
+    f = h5py.File('vgg19_weights.h5')
+    for k in range(f.attrs['nb_layers']):
+        if k >= len(model.layers):
+            break
+        g = f['layer_{}'.format(k)]
+        weights = [g['param_{}'.format(p)] for p in range(g.attrs['nb_params'])]
+        model.layers[k].set_weights(weights)
+    f.close()
+    print('Model loaded.')
+
+    model.add(Dense(10, activation='softmax'))
+
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+# In[3]:
+
+class EarlyStoppingByLossVal(Callback):
+    def __init__(self, monitor='val_loss', value=0.00001, verbose=0):
+        super(Callback, self).__init__()
+        self.monitor = monitor
+        self.value = value
+        self.verbose = verbose
+
+    def on_epoch_end(self, epoch, logs={}):
+        current = logs.get(self.monitor)
+        if current is None:
+            warnings.warn("Early stopping requires %s available!" % self.monitor, RuntimeWarning)
+
+        if current < self.value:
+            if self.verbose > 0:
+                print("Epoch %05d: early stopping THR" % epoch)
+            self.model.stop_training = True
+
+
+# In[4]:
+
+model = VGG_19()
+
+
+# In[5]:
+
+def preProcess(x):
+    mean_pixel = [103.939, 116.779, 123.68]
+    x[0, :, :] -= mean_pixel[0]
+    x[1, :, :] -= mean_pixel[1]
+    x[2, :, :] -= mean_pixel[2]
+    return x
+
+
+# In[6]:
+
+# this is the augmentation configuration we will use for training
+train_data_dir = "Training Data/"
+validation_data_dir = "Validation Data/"
+train_datagen = ImageDataGenerator(
+	                rotation_range=20,
+	                width_shift_range=0.2,
+	                height_shift_range=0.2,
+	                shear_range=0.1,
+	                zoom_range=0.1,
+	                horizontal_flip=False,
+	                fill_mode='nearest',
+                    preprocessing_function = preProcess)
+
+# # this is the augmentation configuration we will use for testing:
+# # only rescaling
+# train_data_dir = '../Labelled Train Data/'
+test_datagen = ImageDataGenerator(preprocessing_function = preProcess)
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+
+validation_generator = test_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=32,
+        class_mode='categorical')
+
+
+
+# In[ ]:
+
+callbacks = [
+        EarlyStoppingByLossVal(monitor='val_loss', value=0.00001, verbose=1),
+        EarlyStopping(monitor='val_loss', patience=5, verbose=0)
+    ]
+
+for e in range(10):
+    model.fit_generator(
+        train_generator,
+        samples_per_epoch=15137,
+        nb_epoch=1,
+        validation_data=validation_generator,
+        nb_val_samples=1681,
+        callbacks = callbacks
+        )
+    model.save("weights-19.h5")
+    print(e)
+
+
+
+
+
